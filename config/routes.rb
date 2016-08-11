0fc9579e319d7fd608918438bf00f5088489aa30
Matt Oakes <mattoakes@mattoakes.local>
2016-08-11 11:42:16 -0700

    Added welcome page plus basic user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :users
+  get 'welcome/index'
+
+  resources :cities
+
+
+root "welcome#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
